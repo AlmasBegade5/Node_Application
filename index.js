1a40cea4914878830f72c5c2ccb2906e598e268b
Almas Begade <almasbegade5@gmail.com>
2021-08-06 09:52:17 +0530

    Simple node application

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect ={
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+}
+
+function solveReact(l,b) {
+    console.log("solving rectangle area and perimeter");
+
+    if(l<=0 || b<=0){
+        console.log("length and breadth must be greater than 0");
+    }
+    else{
+        console.log("Perimeter of rectangle is : " + rect.perimeter(l,b));
+        console.log("Area of rectangle is : " + rect.area(l,b));
+    }
+}
+
+solveReact(2,4);
+solveReact(3,5);
+solveReact(0,5);
+solveReact(5,0);
